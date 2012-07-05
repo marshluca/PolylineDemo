85d2be3214d4d12f40772ebf8075784bd159b6b9
Lin Zhang <marshluca@gmail.com>
2012-07-05 11:03:11 +0800

    create the project with Xcode

---
+++ PolylineDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PolylineDemo
+//
+//  Created by Lin Zhang on 12-7-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
